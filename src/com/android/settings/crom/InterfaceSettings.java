8088de85562391cb9b638ecef9e9899f9722284e
aways <aways@c-rom.org>
2014-05-24 16:03:01 +0200

    derp derp derp
    Signed-off-by: aways <aways@c-rom.org>

--- src/com/android/settings/crom/InterfaceSettings.java
+++ src/com/android/settings/crom/InterfaceSettings.java
@@ -22,6 +22,7 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
 import android.preference.PreferenceCategory;
