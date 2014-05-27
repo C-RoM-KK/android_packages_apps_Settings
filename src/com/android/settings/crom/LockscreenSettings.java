6ea3da832b524250ad05d52549644ec6686bd6ca
aways <aways@c-rom.org>
2014-05-27 22:02:53 +0200

    LS Options ; preference categories and revise strings.
    Credits; LiquidSmooth.

--- src/com/android/settings/crom/LockscreenSettings.java
+++ src/com/android/settings/crom/LockscreenSettings.java
@@ -21,7 +21,10 @@ import android.app.admin.DevicePolicyManager;
 import android.content.pm.PackageManager;
 import android.content.Context;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -41,9 +44,10 @@ public class LockscreenSettings extends SettingsPreferenceFragment
 
     private static final String TAG = "LockscreenSettings";
 
-    private static final String KEY_INTERFACE_SETTINGS = "lock_screen_interface";
-    private static final String KEY_TARGET_SETTINGS = "lock_screen_targets";
-    private static final String KEY_WIDGETS_SETTINGS = "lock_screen_widgets";
+    private static final String KEY_LOCKSCREEN_BUTTONS = "lockscreen_buttons";
+    private static final String KEY_INTERFACE_SETTINGS = "lockscreen_interface";
+    private static final String KEY_TARGET_SETTINGS = "lockscreen_targets";
+    private static final String KEY_WIDGETS_SETTINGS = "lockscreen_widgets";
     private static final String KEY_GENERAL_CATEGORY = "general_category";
     private static final String KEY_BATTERY_AROUND_RING = "battery_around_ring";
     private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
@@ -51,6 +55,8 @@ public class LockscreenSettings extends SettingsPreferenceFragment
     private static final String KEY_LOCK_BEFORE_UNLOCK = "lock_before_unlock";
     private static final String KEY_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
     private static final String KEY_MENU_UNLOCK_PREF = "menu_unlock";
+    private static final String KEY_SHAKE_TO_SECURE = "shake_to_secure";
+    private static final String KEY_SHAKE_AUTO_TIMEOUT = "shake_auto_timeout";
 
     private PackageManager mPM;
     private DevicePolicyManager mDPM;
@@ -62,6 +68,8 @@ public class LockscreenSettings extends SettingsPreferenceFragment
     private CheckBoxPreference mLockBeforeUnlock;
     private CheckBoxPreference mLockQuickUnlock;
     private CheckBoxPreference mMenuUnlock;
+    private CheckBoxPreference mShakeToSecure;
+    private ListPreference mShakeTimer;
 
     // needed for menu unlock
     private Resources keyguardResource;
@@ -89,6 +97,25 @@ public class LockscreenSettings extends SettingsPreferenceFragment
             mLockBeforeUnlock.setChecked(Settings.Secure.getInt(getContentResolver(),
                     Settings.Secure.LOCK_BEFORE_UNLOCK, 0) == 1);
             mLockBeforeUnlock.setOnPreferenceChangeListener(this);
+        }
+
+        mShakeToSecure = (CheckBoxPreference) prefs
+                .findPreference(KEY_SHAKE_TO_SECURE);
+        if (mShakeToSecure != null) {
+            mShakeToSecure.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SHAKE_TEMP_SECURE, 0) == 1);
+            mShakeToSecure.setOnPreferenceChangeListener(this);
+        }
+
+        mShakeTimer = (ListPreference) prefs
+                .findPreference(KEY_SHAKE_AUTO_TIMEOUT);
+        if (mShakeTimer != null) {
+            long shakeTimer = Settings.Secure.getLongForUser(getContentResolver(),
+                    Settings.Secure.LOCK_SHAKE_SECURE_TIMER, 0,
+                    UserHandle.USER_CURRENT);
+            mShakeTimer.setValue(String.valueOf(shakeTimer));
+            updateShakeTimerPreferenceSummary();
+            mShakeTimer.setOnPreferenceChangeListener(this);
         }
 
         mLockQuickUnlock = (CheckBoxPreference) prefs
@@ -181,6 +208,31 @@ public class LockscreenSettings extends SettingsPreferenceFragment
                 mMenuUnlock.setOnPreferenceChangeListener(this);
             }
         }
+
+        PreferenceScreen lockButtons = (PreferenceScreen) prefs
+                .findPreference(KEY_LOCKSCREEN_BUTTONS);
+        boolean hasButtons = (getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys) > 0);
+        if (!hasButtons) {
+            generalCategory.removePreference(lockButtons);
+        }
+    }
+
+    private void updateShakeTimerPreferenceSummary() {
+        // Update summary message with current value
+        long shakeTimer = Settings.Secure.getLongForUser(getContentResolver(),
+                Settings.Secure.LOCK_SHAKE_SECURE_TIMER, 0,
+                UserHandle.USER_CURRENT);
+        final CharSequence[] entries = mShakeTimer.getEntries();
+        final CharSequence[] values = mShakeTimer.getEntryValues();
+        int best = 0;
+        for (int i = 0; i < values.length; i++) {
+            long timeout = Long.valueOf(values[i].toString());
+            if (shakeTimer >= timeout) {
+                best = i;
+            }
+        }
+        mShakeTimer.setSummary(entries[best]);
     }
 
     @Override
@@ -189,6 +241,18 @@ public class LockscreenSettings extends SettingsPreferenceFragment
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.LOCK_BEFORE_UNLOCK,
                     ((Boolean) value) ? 1 : 0);
+        } else if (preference == mShakeToSecure) {
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.LOCK_SHAKE_TEMP_SECURE, 0);
+        } else if (preference == mShakeTimer) {
+            int shakeTime = Integer.parseInt((String) value);
+            try {
+                Settings.Secure.putInt(getContentResolver(),
+                        Settings.Secure.LOCK_SHAKE_SECURE_TIMER, shakeTime);
+            } catch (NumberFormatException e) {
+                Log.e("LockscreenSettings", "could not persist lockAfter timeout setting", e);
+            }
+            updateShakeTimerPreferenceSummary();
         } else if (preference == mBatteryStatus) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY,
