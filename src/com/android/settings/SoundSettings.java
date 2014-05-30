SlimRoms/packages_apps_Settings

Settings: Pulse Torch On Incoming Call (3/4)
Remove SeekBarPreference from Settings
	and change all references to the internal
	preference (no code changed)

Change-Id: Ia21486157d8998baecd8f9ff0624b6e78a13831c

src/com/android/settings/SoundSettings.java
@@ -44,13 +44,12 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.VolumePanel;
-
-import com.android.settings.widget.SeekBarPreference;
 
 import java.util.List;
 
@@ -95,7 +94,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mDtmfTone;
     private CheckBoxPreference mSoundEffects;
     private CheckBoxPreference mHapticFeedback;
-    private SeekBarPreference mVibrationDuration;
+    private SlimSeekBarPreference mVibrationDuration;
     private Preference mMusicFx;
     private CheckBoxPreference mLockSounds;
     private Preference mRingtonePreference;
@@ -185,7 +184,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                 Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0);
         int userMillis = Settings.System.getInt(resolver,
                 Settings.System.MINIMUM_VIBRATION_DURATION, 0);
-        mVibrationDuration = (SeekBarPreference) findPreference(KEY_VIBRATION_DURATION);
+        mVibrationDuration = (SlimSeekBarPreference) findPreference(KEY_VIBRATION_DURATION);
         mVibrationDuration.setInterval(1);
         mVibrationDuration.setDefault(0);
         mVibrationDuration.isMilliseconds(true);

src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -39,6 +39,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.TextUtils.SimpleStringSplitter;
@@ -57,7 +58,6 @@ import com.android.settings.DialogCreatable;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settings.widget.SeekBarPreference;
 
 import java.util.HashSet;
 import java.util.List;
@@ -203,7 +203,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mToggleLockScreenRotationPreference;
     private CheckBoxPreference mToggleSpeakPasswordPreference;
     private ListPreference mShakeSensitivity;
-    private SeekBarPreference mSelectLongPressTimeoutPreference;
+    private SlimSeekBarPreference mSelectLongPressTimeoutPreference;
     private Preference mNoServicesMessagePreference;
     private PreferenceScreen mCaptioningPreferenceScreen;
     private PreferenceScreen mDisplayMagnificationPreferenceScreen;
@@ -387,7 +387,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
 
         // Long press timeout.
         mSelectLongPressTimeoutPreference =
-                (SeekBarPreference) findPreference(SELECT_LONG_PRESS_TIMEOUT_PREFERENCE);
+                (SlimSeekBarPreference) findPreference(SELECT_LONG_PRESS_TIMEOUT_PREFERENCE);
         mSelectLongPressTimeoutPreference.setDefault(LONGPRESS_TIME_DEFAULT);
         mSelectLongPressTimeoutPreference.isMilliseconds(true);
         mSelectLongPressTimeoutPreference.setInterval(1);

src/com/android/settings/slim/NotificationDrawerStyle.java
@@ -31,6 +31,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -41,7 +42,6 @@ import com.android.internal.util.slim.DeviceUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import java.io.File;
 
@@ -63,7 +63,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
 
     private ListPreference mNotificationWallpaper;
     private ListPreference mNotificationWallpaperLandscape;
-    SeekBarPreference mWallpaperAlpha;
+    SlimSeekBarPreference mWallpaperAlpha;
 
     private File mImageTmp;
 
@@ -105,7 +105,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
             Settings.System.putFloat(getContentResolver(),
                     Settings.System.NOTIFICATION_BACKGROUND_ALPHA, 0.1f);
         }
-        mWallpaperAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
+        mWallpaperAlpha = (SlimSeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
         mWallpaperAlpha.setInitValue((int) (transparency * 100));
         mWallpaperAlpha.setOnPreferenceChangeListener(this);
 

src/com/android/settings/slim/PieButtonStyleSettings.java
@@ -28,6 +28,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -36,7 +37,6 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -73,8 +73,8 @@ public class PieButtonStyleSettings extends SettingsPreferenceFragment implement
     ColorPickerPreference mPieButtonOutlineColor;
     ColorPickerPreference mPieIconColor;
     ListPreference mPieIconColorMode;
-    SeekBarPreference mPieButtonAlpha;
-    SeekBarPreference mPieButtonPressedAlpha;
+    SlimSeekBarPreference mPieButtonAlpha;
+    SlimSeekBarPreference mPieButtonPressedAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -116,11 +116,11 @@ public class PieButtonStyleSettings extends SettingsPreferenceFragment implement
         mPieIconColor.setOnPreferenceChangeListener(this);
 
         mPieButtonAlpha =
-                (SeekBarPreference) findPreference(PREF_PIE_BUTTON_ALPHA);
+                (SlimSeekBarPreference) findPreference(PREF_PIE_BUTTON_ALPHA);
         mPieButtonAlpha.setOnPreferenceChangeListener(this);
 
         mPieButtonPressedAlpha =
-                (SeekBarPreference) findPreference(PREF_PIE_BUTTON_PRESSED_ALPHA);
+                (SlimSeekBarPreference) findPreference(PREF_PIE_BUTTON_PRESSED_ALPHA);
         mPieButtonPressedAlpha.setOnPreferenceChangeListener(this);
 
         mPieIconColorMode =

src/com/android/settings/slim/PieStyleSettings.java
@@ -28,6 +28,7 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -36,7 +37,6 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -67,11 +67,11 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mPieBackgroundColor;
     private ColorPickerPreference mPieSnapColor;
     private ColorPickerPreference mPieTextColor;
-    private SeekBarPreference mPieBackgroundAlpha;
+    private SlimSeekBarPreference mPieBackgroundAlpha;
     private CheckBoxPreference mShowSnap;
     private CheckBoxPreference mShowText;
     private CheckBoxPreference mShowBackground;
-    private SeekBarPreference mPieControlSize;
+    private SlimSeekBarPreference mPieControlSize;
     private CheckBoxPreference mMirrorRightPie;
 
     @Override
@@ -101,7 +101,7 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
         mPieTextColor = (ColorPickerPreference) findPreference(PREF_PIE_TEXT_COLOR);
         mPieTextColor.setOnPreferenceChangeListener(this);
 
-        mPieBackgroundAlpha = (SeekBarPreference) findPreference(PREF_PIE_BACKGROUND_ALPHA);
+        mPieBackgroundAlpha = (SlimSeekBarPreference) findPreference(PREF_PIE_BACKGROUND_ALPHA);
         mPieBackgroundAlpha.setOnPreferenceChangeListener(this);
 
         mShowSnap = (CheckBoxPreference) findPreference(PREF_PIE_SHOW_SNAP);
@@ -113,7 +113,7 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
         mShowBackground = (CheckBoxPreference) findPreference(PREF_PIE_SHOW_BACKGROUND);
         mShowBackground.setOnPreferenceChangeListener(this);
 
-        mPieControlSize = (SeekBarPreference) findPreference(PREF_PIE_CONTROL_SIZE);
+        mPieControlSize = (SlimSeekBarPreference) findPreference(PREF_PIE_CONTROL_SIZE);
         mPieControlSize.setOnPreferenceChangeListener(this);
 
         mMirrorRightPie = (CheckBoxPreference) findPreference(PREF_PIE_MIRROR_RIGHT);

src/com/android/settings/slim/quicksettings/QuickSettingsTilesStyle.java
@@ -29,6 +29,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -39,7 +40,6 @@ import com.android.internal.util.slim.DeviceUtils;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -74,7 +74,7 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
     private ColorPickerPreference mQuickTilesBgColor;
     private ColorPickerPreference mQuickTilesBgPressedColor;
     private ColorPickerPreference mQuickTilesTextColor;
-    private SeekBarPreference mQsTileAlpha;
+    private SlimSeekBarPreference mQsTileAlpha;
 
     private boolean mCheckPreferences;
 
@@ -160,7 +160,7 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
                     Settings.System.QUICK_TILES_BG_ALPHA, 0.0f);
         }
         if (mQsTileAlpha == null) {
-            mQsTileAlpha = (SeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
+            mQsTileAlpha = (SlimSeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
             mQsTileAlpha.setInitValue((int) (transparency * 100));
             mQsTileAlpha.setOnPreferenceChangeListener(this);
         }

C-RoM-KK/android_packages_apps_Settings

Conflicts:
	res/xml/notification_drawer_qs_settings.xml

src/com/android/settings/SoundSettings.java
@@ -44,13 +44,12 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.VolumePanel;
-
-import com.android.settings.widget.SeekBarPreference;
 
 import java.util.List;
 
@@ -95,7 +94,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mDtmfTone;
     private CheckBoxPreference mSoundEffects;
     private CheckBoxPreference mHapticFeedback;
-    private SeekBarPreference mVibrationDuration;
+    private CromSeekBarPreference mVibrationDuration;
     private Preference mMusicFx;
     private CheckBoxPreference mLockSounds;
     private Preference mRingtonePreference;
@@ -185,7 +184,7 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                 Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0);
         int userMillis = Settings.System.getInt(resolver,
                 Settings.System.MINIMUM_VIBRATION_DURATION, 0);
-        mVibrationDuration = (SeekBarPreference) findPreference(KEY_VIBRATION_DURATION);
+        mVibrationDuration = (CromSeekBarPreference) findPreference(KEY_VIBRATION_DURATION);
         mVibrationDuration.setInterval(1);
         mVibrationDuration.setDefault(0);
         mVibrationDuration.isMilliseconds(true);

src/com/android/settings/accessibility/AccessibilitySettings.java
@@ -39,6 +39,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.text.TextUtils.SimpleStringSplitter;
@@ -56,7 +57,6 @@ import com.android.settings.DialogCreatable;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
-import com.android.settings.widget.SeekBarPreference;
 
 import java.util.HashSet;
 import java.util.List;
@@ -196,7 +196,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
     private CheckBoxPreference mToggleLockScreenRotationPreference;
     private CheckBoxPreference mToggleSpeakPasswordPreference;
     private ListPreference mShakeSensitivity;
-    private SeekBarPreference mSelectLongPressTimeoutPreference;
+    private CromSeekBarPreference mSelectLongPressTimeoutPreference;
     private Preference mNoServicesMessagePreference;
     private PreferenceScreen mCaptioningPreferenceScreen;
     private PreferenceScreen mDisplayMagnificationPreferenceScreen;
@@ -362,7 +362,7 @@ public class AccessibilitySettings extends SettingsPreferenceFragment implements
 
         // Long press timeout.
         mSelectLongPressTimeoutPreference =
-                (SeekBarPreference) findPreference(SELECT_LONG_PRESS_TIMEOUT_PREFERENCE);
+                (CromSeekBarPreference) findPreference(SELECT_LONG_PRESS_TIMEOUT_PREFERENCE);
         mSelectLongPressTimeoutPreference.setDefault(LONGPRESS_TIME_DEFAULT);
         mSelectLongPressTimeoutPreference.isMilliseconds(true);
         mSelectLongPressTimeoutPreference.setInterval(1);

src/com/android/settings/crom/NotificationDrawerStyle.java
@@ -31,6 +31,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -41,7 +42,6 @@ import com.android.internal.util.slim.DeviceUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import java.io.File;
 
@@ -63,7 +63,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
 
     private ListPreference mNotificationWallpaper;
     private ListPreference mNotificationWallpaperLandscape;
-    SeekBarPreference mWallpaperAlpha;
+    CromSeekBarPreference mWallpaperAlpha;
 
     private File mImageTmp;
 
@@ -105,7 +105,7 @@ public class NotificationDrawerStyle extends SettingsPreferenceFragment implemen
             Settings.System.putFloat(getContentResolver(),
                     Settings.System.NOTIFICATION_BACKGROUND_ALPHA, 0.1f);
         }
-        mWallpaperAlpha = (SeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
+        mWallpaperAlpha = (CromSeekBarPreference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
         mWallpaperAlpha.setInitValue((int) (transparency * 100));
         mWallpaperAlpha.setOnPreferenceChangeListener(this);
 

src/com/android/settings/crom/PieButtonStyleSettings.java
@@ -28,6 +28,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -36,7 +37,6 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -73,8 +73,8 @@ public class PieButtonStyleSettings extends SettingsPreferenceFragment implement
     ColorPickerPreference mPieButtonOutlineColor;
     ColorPickerPreference mPieIconColor;
     ListPreference mPieIconColorMode;
-    SeekBarPreference mPieButtonAlpha;
-    SeekBarPreference mPieButtonPressedAlpha;
+    CromSeekBarPreference mPieButtonAlpha;
+    CromSeekBarPreference mPieButtonPressedAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -116,11 +116,11 @@ public class PieButtonStyleSettings extends SettingsPreferenceFragment implement
         mPieIconColor.setOnPreferenceChangeListener(this);
 
         mPieButtonAlpha =
-                (SeekBarPreference) findPreference(PREF_PIE_BUTTON_ALPHA);
+                (CromSeekBarPreference) findPreference(PREF_PIE_BUTTON_ALPHA);
         mPieButtonAlpha.setOnPreferenceChangeListener(this);
 
         mPieButtonPressedAlpha =
-                (SeekBarPreference) findPreference(PREF_PIE_BUTTON_PRESSED_ALPHA);
+                (CromSeekBarPreference) findPreference(PREF_PIE_BUTTON_PRESSED_ALPHA);
         mPieButtonPressedAlpha.setOnPreferenceChangeListener(this);
 
         mPieIconColorMode =

src/com/android/settings/crom/PieStyleSettings.java
@@ -28,6 +28,7 @@ import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -36,7 +37,6 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -67,11 +67,11 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
     private ColorPickerPreference mPieBackgroundColor;
     private ColorPickerPreference mPieSnapColor;
     private ColorPickerPreference mPieTextColor;
-    private SeekBarPreference mPieBackgroundAlpha;
+    private CromSeekBarPreference mPieBackgroundAlpha;
     private CheckBoxPreference mShowSnap;
     private CheckBoxPreference mShowText;
     private CheckBoxPreference mShowBackground;
-    private SeekBarPreference mPieControlSize;
+    private CromSeekBarPreference mPieControlSize;
     private CheckBoxPreference mMirrorRightPie;
 
     @Override
@@ -101,7 +101,7 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
         mPieTextColor = (ColorPickerPreference) findPreference(PREF_PIE_TEXT_COLOR);
         mPieTextColor.setOnPreferenceChangeListener(this);
 
-        mPieBackgroundAlpha = (SeekBarPreference) findPreference(PREF_PIE_BACKGROUND_ALPHA);
+        mPieBackgroundAlpha = (CromSeekBarPreference) findPreference(PREF_PIE_BACKGROUND_ALPHA);
         mPieBackgroundAlpha.setOnPreferenceChangeListener(this);
 
         mShowSnap = (CheckBoxPreference) findPreference(PREF_PIE_SHOW_SNAP);
@@ -113,7 +113,7 @@ public class PieStyleSettings extends SettingsPreferenceFragment implements
         mShowBackground = (CheckBoxPreference) findPreference(PREF_PIE_SHOW_BACKGROUND);
         mShowBackground.setOnPreferenceChangeListener(this);
 
-        mPieControlSize = (SeekBarPreference) findPreference(PREF_PIE_CONTROL_SIZE);
+        mPieControlSize = (CromSeekBarPreference) findPreference(PREF_PIE_CONTROL_SIZE);
         mPieControlSize.setOnPreferenceChangeListener(this);
 
         mMirrorRightPie = (CheckBoxPreference) findPreference(PREF_PIE_MIRROR_RIGHT);

src/com/android/settings/crom/quicksettings/QuickSettingsTilesStyle.java
@@ -29,6 +29,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
@@ -39,7 +40,6 @@ import com.android.internal.util.slim.DeviceUtils;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import com.android.settings.widget.SeekBarPreference;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
@@ -74,7 +74,7 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
     private ColorPickerPreference mQuickTilesBgColor;
     private ColorPickerPreference mQuickTilesBgPressedColor;
     private ColorPickerPreference mQuickTilesTextColor;
-    private SeekBarPreference mQsTileAlpha;
+    private CromSeekBarPreference mQsTileAlpha;
 
     private boolean mCheckPreferences;
 
@@ -160,7 +160,7 @@ public class QuickSettingsTilesStyle extends SettingsPreferenceFragment implemen
                     Settings.System.QUICK_TILES_BG_ALPHA, 0.0f);
         }
         if (mQsTileAlpha == null) {
-            mQsTileAlpha = (SeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
+            mQsTileAlpha = (CromSeekBarPreference) findPreference(PREF_QUICK_TILES_ALPHA);
             mQsTileAlpha.setInitValue((int) (transparency * 100));
             mQsTileAlpha.setOnPreferenceChangeListener(this);
         }