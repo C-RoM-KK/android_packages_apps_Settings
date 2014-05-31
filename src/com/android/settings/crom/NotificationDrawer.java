1340fbd5722c9754df644121d756d0f740878044
aways <aways@c-rom.org>
2014-06-01 01:07:03 +0200

    add needed import
    Signed-off-by: aways <aways@c-rom.org>

--- src/com/android/settings/crom/NotificationDrawer.java
+++ src/com/android/settings/crom/NotificationDrawer.java
@@ -20,6 +20,7 @@ import android.app.Activity;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
