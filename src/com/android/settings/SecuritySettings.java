492fbdac8dab4d7d25e7ff59501a724e83679db6
aways <aways@c-rom.org>
2014-06-22 14:06:09 +0200

    Revert "Re-Enable power menu"
    This reverts commit a58a9a7e0c5b95943f8639db183b0f5dab5b72bd.

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -370,7 +370,7 @@ public class SecuritySettings extends RestrictedSettingsFragment
 
         mAdvancedReboot = (ListPreference) root.findPreference(KEY_ADVANCED_REBOOT);
         mAdvancedReboot.setValue(String.valueOf(Settings.Secure.getInt(
-                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 2)));
+                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 0)));
         mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
         mAdvancedReboot.setOnPreferenceChangeListener(this);
 
