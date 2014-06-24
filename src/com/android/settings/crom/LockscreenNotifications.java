9f97b9d0a67ecc48aa0dacfb2951296d1cba7b7b
aways <aways@c-rom.org>
2014-06-24 22:57:09 +0200

    fix error

--- src/com/android/settings/crom/LockscreenNotifications.java
+++ src/com/android/settings/crom/LockscreenNotifications.java
@@ -223,7 +223,6 @@ public class LockscreenNotifications extends SettingsPreferenceFragment
         mDynamicWidth.setChecked(Settings.System.getInt(cr,
                     Settings.System.LOCKSCREEN_NOTIFICATIONS_DYNAMIC_WIDTH, 0) == 1);
         mDynamicWidth.setEnabled(mLockscreenNotifications.isChecked());
-        }
 
         if (mPackageStatusReceiver == null) {
             mPackageStatusReceiver = new PackageStatusReceiver();
