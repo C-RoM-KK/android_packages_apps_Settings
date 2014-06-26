TheKang/packages_apps_Settings

HeadsUp: User configurable timeout [2/2]
Change-Id: I6c55636205237044891324f0dd855046aaafb256

src/com/android/settings/headsup/HeadsUpSettings.java
@@ -33,6 +33,7 @@ import android.os.Handler;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
+import android.preference.SlimSeekBarPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.*;
@@ -48,12 +49,17 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final int DIALOG_DND_APPS = 0;
     private static final int DIALOG_BLACKLIST_APPS = 1;
 
+    private static final String KEY_HEADS_UP_TIMEOUT = "heads_up_timeout";
+    private static final String KEY_HEADS_UP_FS_TIMEOUT = "heads_up_fullscreen_timeout";
+
     private PackageAdapter mPackageAdapter;
     private PackageManager mPackageManager;
     private PreferenceGroup mDndPrefList;
     private PreferenceGroup mBlacklistPrefList;
     private Preference mAddDndPref;
     private Preference mAddBlacklistPref;
+    private SlimSeekBarPreference mHeadsUpTimeout;
+    private SlimSeekBarPreference mHeadsUpFSTimeout;
 
     private String mDndPackageList;
     private String mBlacklistPackageList;
@@ -76,6 +82,16 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         mBlacklistPrefList = (PreferenceGroup) findPreference("blacklist_applications");
         mBlacklistPrefList.setOrderingAsAdded(false);
+
+        mHeadsUpTimeout = (SlimSeekBarPreference) findPreference(KEY_HEADS_UP_TIMEOUT);
+        mHeadsUpTimeout.setInitValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, 3700));
+        mHeadsUpTimeout.setOnPreferenceChangeListener(this);
+
+        mHeadsUpFSTimeout = (SlimSeekBarPreference) findPreference(KEY_HEADS_UP_FS_TIMEOUT);
+        mHeadsUpFSTimeout.setInitValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_FS_TIMEOUT, 700));
+        mHeadsUpFSTimeout.setOnPreferenceChangeListener(this);
 
         mDndPackages = new HashMap<String, Package>();
         mBlacklistPackages = new HashMap<String, Package>();
@@ -392,6 +408,24 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         if (preference == mAddBlacklistPref) {
             showDialog(DIALOG_BLACKLIST_APPS);
+        }
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mEnabledPref) {
+            getActivity().invalidateOptionsMenu();
+        } else if (preference == mHeadsUpTimeout) {
+            int length = ((Integer) objValue).intValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT, length);
+        } else if (preference == mHeadsUpFSTimeout) {
+            int length = ((Integer) objValue).intValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_FS_TIMEOUT, length);
+        } else {
+            Preference pref = preference;
+            updateValues(pref.getKey());
         }
         return true;
     }

C-RoM-KK/android_packages_apps_Settings

src/com/android/settings/headsup/HeadsUpSettings.java
@@ -33,6 +33,7 @@ import android.os.Handler;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
+import android.preference.CromSeekBarPreference;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.view.*;
@@ -48,12 +49,17 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final int DIALOG_DND_APPS = 0;
     private static final int DIALOG_BLACKLIST_APPS = 1;
 
+    private static final String KEY_HEADS_UP_TIMEOUT = "heads_up_timeout";
+    private static final String KEY_HEADS_UP_FS_TIMEOUT = "heads_up_fullscreen_timeout";
+
     private PackageAdapter mPackageAdapter;
     private PackageManager mPackageManager;
     private PreferenceGroup mDndPrefList;
     private PreferenceGroup mBlacklistPrefList;
     private Preference mAddDndPref;
     private Preference mAddBlacklistPref;
+    private CromSeekBarPreference mHeadsUpTimeout;
+    private CromSeekBarPreference mHeadsUpFSTimeout;
 
     private String mDndPackageList;
     private String mBlacklistPackageList;
@@ -76,6 +82,16 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         mBlacklistPrefList = (PreferenceGroup) findPreference("blacklist_applications");
         mBlacklistPrefList.setOrderingAsAdded(false);
+
+        mHeadsUpTimeout = (CromSeekBarPreference) findPreference(KEY_HEADS_UP_TIMEOUT);
+        mHeadsUpTimeout.setInitValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_TIMEOUT, 3700));
+        mHeadsUpTimeout.setOnPreferenceChangeListener(this);
+
+        mHeadsUpFSTimeout = (CromSeekBarPreference) findPreference(KEY_HEADS_UP_FS_TIMEOUT);
+        mHeadsUpFSTimeout.setInitValue(Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_FS_TIMEOUT, 700));
+        mHeadsUpFSTimeout.setOnPreferenceChangeListener(this);
 
         mDndPackages = new HashMap<String, Package>();
         mBlacklistPackages = new HashMap<String, Package>();
@@ -392,6 +408,24 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
 
         if (preference == mAddBlacklistPref) {
             showDialog(DIALOG_BLACKLIST_APPS);
+        }
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mEnabledPref) {
+            getActivity().invalidateOptionsMenu();
+        } else if (preference == mHeadsUpTimeout) {
+            int length = ((Integer) objValue).intValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_TIMEOUT, length);
+        } else if (preference == mHeadsUpFSTimeout) {
+            int length = ((Integer) objValue).intValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_FS_TIMEOUT, length);
+        } else {
+            Preference pref = preference;
+            updateValues(pref.getKey());
         }
         return true;
     }