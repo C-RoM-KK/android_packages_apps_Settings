205ac7643281a7309c718944e1e9940a4da6a7dc
aways <aways@c-rom.org>
2014-06-29 01:22:27 +0200

    Revert "Again fix"
    This reverts commit fc4ae3efefe3113187843a038236aa0bc4fa8272.

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -23,7 +23,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
